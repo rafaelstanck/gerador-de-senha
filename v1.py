05f74ea0b11d644583e41bddd80a92c45af1f0c8
Rafael Stanck <86021032+rafaelstanck@users.noreply.github.com>
2022-03-15 16:25:36 -0300

    Criação da versão 2.0 do gerador de senhas

--- v1.py
+++ v1.py
@@ -7,19 +7,19 @@ from random import choice, randint, shuffle
 
 def gerasenha():
 
-    c1 = r'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%&*+?()[]{}|=/",.;:^_-~'
+    caracter = r'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%&*+?()[]{}|=/",.;:^_-~'  # caracteres a serem utilizados
 
-    senha = [choice(c1[:26]), choice(c1[0:26]).upper(), choice(c1[26:36]), choice(c1[36:])]
+    senha = [choice(caracter[:26]), choice(caracter[0:26]).upper(), choice(caracter[26:36]), choice(caracter[36:])]
 
     while len(senha) <= 7:
         for i4 in range(1, randint(1, 2)):
-            senha.append(choice(c1[36:]))
+            senha.append(choice(caracter[36:]))
         for i3 in range(1, randint(1, 2)):
-            senha.append(choice(c1[26:36]))
+            senha.append(choice(caracter[26:36]))
         for i2 in range(1, randint(1, 2)):
-            senha.append(choice(c1[:26]).upper())
+            senha.append(choice(caracter[:26]).upper())
         for i1 in range(0, (8-len(senha))):
-            senha.append(choice(c1[:26]))
+            senha.append(choice(caracter[:26]))
     shuffle(senha)
     print(''.join(senha))
 
---
+++ v2.py
@@ -0,0 +1,37 @@
+"""
+Gerador de Senha Versão 2.0
+Gera senhas de forma customizada, podendo ser apenas números, letras, caracteres especiais ou tudo junto.
+"""
+from random import choice, shuffle
+
+
+def gerasenha(a=3, b=3, c=3):
+    """
+    Esta função permite o usuário escolher a quantidade dos caracteres que formarão a senha, sendo:
+    a = quantidade de letras (maiúscula ou minúscula)
+    b = quantidade de números
+    c = quantidade de caracteres especiais
+    Colocar o valor 0 (zero) caso não queira letras, números ou caracteres especiais.
+    """
+
+    letras = 'ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz'
+    numeros = '0123456789'
+    caracteres_especiais = r'\|".,;:!@#$%¨&*()+<>?[]{}/-_'
+    senha = []
+
+    for i in range(a):  # iterador para acrescentar letras
+        senha.append(choice(letras))
+
+    for i in range(b):  # iterador para acrescentar números
+        senha.append(choice(numeros))
+
+    for i in range(c):  # iterador para acrescentar caracteres especiais
+        senha.append(choice(caracteres_especiais))
+
+    shuffle(senha)  # função para embaralhar os caracteres escolhidos
+
+    return ''.join(senha)  # returna a senha no formato de uma string
+
+
+senhanova = gerasenha(0, 0, 6)
+print(senhanova)
